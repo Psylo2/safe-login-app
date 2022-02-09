e8840c1fe900c8c209668c5268d16c46547d24a7
Psylo2 <psylo20@hotmail.com>
2022-02-10 01:25:26 +0200

    change logics

--- logic/admin_logic.py
+++ logic/admin_logic.py
@@ -1,47 +1,68 @@
-from flask import request, url_for, redirect, flash, render_template, session
+from typing import Dict
 
-from logic.user_logic import Password
+from flask import flash
+
+from usecases import AdminUseCase
+from logic.services import AdminLogicService
+
 from models.password.password import PasswordConfig
 from models.user import User
 
 
-def users_list():
-    _users = User.find_all_from_db()
-    return render_template('admin/user_list.html', users=_users)
+class AdminLogic(AdminUseCase, AdminLogicService):
+    def __init__(self):
+        pass
 
+    def users_list(self):
+        all_users = User.find_all_from_db()
+        return all_users
 
-def block_user(block):
-    for user in User.find_all_from_db():
-        if len(list(set(block) & set(user._name))) == 0:
-            user.block_user_model(block)
+    def block_user(self, block) -> bool:
+        for user in User.find_all_from_db():
+            if len(list(set(block) & set(user._name))) == 0:
+                user.block_user_model(block)
+                return True
+        return False
 
-def unblock_user(unblock):
-    for user in User.find_all_from_db():
-        if len(list(set(unblock) & set(user._name))) == 0:
-            user.unblock_user_model(unblock)
+    def unblock_user(self, unblock) -> bool:
+        for user in User.find_all_from_db():
+            if len(list(set(unblock) & set(user._name))) == 0:
+                user.unblock_user_model(unblock)
+                return True
+            return False
 
+    def password_configuration(self, upper: bool, lower: bool, digits: bool,
+                               spec: bool, use_dict: bool, length: str,
+                               history: str, tries: str):
 
-def password_configuration(upper: bool, lower: bool, digits: bool,
-                           spec: bool, use_dict: bool, length: str,
-                           history: str, tries: str):
-    re = ""
-    try:
+        configurations = self.extract_configuration_fields(upper=upper, lower=lower, digits=digits, spec=spec,
+                                                           use_dict=use_dict, length=length, history=history,
+                                                           tries=tries)
+        result = PasswordConfig.set_config(**configurations)
+        if result:
+            flash('Password Configuration Changed!', 'danger')
+            return True
+
+        flash('Invalid Inputs', 'danger')
+        return False
+
+    def configure_regex(self, upper, lower, digits, spec) -> str:
+        re = ""
         re += "A-Z" if upper else ""
         re += "a-z" if lower else ""
         re += "0-9" if digits else ""
         re += "\!\#\$\%\^\&\*\_\+\.\," if spec else ""
+        return re
 
-
-        PasswordConfig._set_config(length=length,
-                             regex=re,
-                             history=history,
-                             dictionary=use_dict,
-                             tries=tries)
-
-
-        flash('Password Configuration Changed!', 'danger')
-
-    except Exception as e:
-        print(e)
-        flash('Invalid Inputs', 'danger')
-    return redirect(url_for('admin.password_conf_get'))
+    def extract_configuration_fields(self, upper: bool, lower: bool, digits: bool,
+                                     spec: bool, use_dict: bool, length: str,
+                                     history: str, tries: str) -> Dict:
+        regex = self.configure_regex(upper=upper, lower=lower, digits=digits, spec=spec)
+        use_dict = True if use_dict == "1" else False
+        return {
+            "dictionary": use_dict,
+            "regex": regex,
+            "length": length,
+            "history": history,
+            "tries": tries
+        }
--- logic/user_logic.py
+++ logic/user_logic.py
@@ -1,85 +1,79 @@
-from flask import url_for, redirect, flash, session
+from flask import flash, session
+
+from db.db import _decrypt_
+from usecases import UserUseCase
+
 from models.user import User
 from models.password import Password
-from db.db import _decrypt_
 
 
-def _login(name_email: str, password: str):
-    try:
-        users = User.find_all_from_db()
-        for user in users:
-            if _decrypt_(name_email, user._name):
-                if user._blocked == 0:
-                    user._name = name_email
-                    for pas in Password.find_all_from_db():
-                        if _decrypt_(name_email, pas._username):
-                            if Password.confirm_password(password, name_email):
-                                if _decrypt_(password, pas._current_password):
-                                    session.update(name_email=user._name)
+class UserLogic(UserUseCase):
+    def __init__(self):
+        pass
 
-                                    flash(f'Welcome {user._name}', 'danger')
-                                    return redirect(url_for('home'))
-                                break
-                            flash(f'Password Security Protocol has Updated.\n'
-                                  f'Change you Password Please!', 'danger')
-                            return redirect(url_for('users.change_password_get'))
+    def login(self, name_email: str, password: str):
+        try:
+            users = User.find_all_from_db()
+            for user in users:
+                if _decrypt_(name_email, user._name):
+                    if user._blocked == 0:
+                        user._name = name_email
+                        for pas in Password.find_all_from_db():
+                            if _decrypt_(name_email, pas._username):
+                                if Password.confirm_password(password, name_email):
+                                    if _decrypt_(password, pas._current_password):
+                                        session.update(name_email=user._name)
 
-        flash('User not Exists\nor\nPassword dont meet complexity!',
-              'danger')
+                                        flash(f'Welcome {user._name}', 'danger')
+                                        return True
+                                    break
+                                flash(f'Password Security Protocol has Updated.\n'
+                                      f'Change you Password Please!', 'danger')
 
-    except Exception as e:
-        print(e)
-        flash('Invalid Inputs', 'danger')
+            flash('User not Exists\nor\nPassword dont meet complexity!',
+                  'danger')
 
-    return redirect(url_for('users.login_get'))
+        except Exception:
+            flash('Invalid Inputs', 'danger')
 
+    def register(self, username: str, email: str, password: str) -> bool:
+        try:
+            users = User.find_all_from_db()
+            exists = False
+            for user in users:
+                if _decrypt_(username, user._name):
+                    exists = True
+            if not exists:
+                if Password.confirm_password(password, update=False):
+                    user = User(username, email, 0)
+                    user.save_to_db(password)
 
-def _register(username: str, email: str,
-              password: str):
-    try:
-        users = User.find_all_from_db()
-        exists = False
-        for user in users:
-            if _decrypt_(username, user._name):
-                exists = True
-        if not exists:
-            if Password.confirm_password(password, update=False):
-                user = User(username, email, 0)
-                user.save_to_db(password)
+                    flash("Successful Registration", "danger")
+                    return True
 
-                flash("Successful Registration", "danger")
-                return redirect(url_for('users.login_get'))
+            flash('Password dont meet complexity!', 'danger')
 
-        flash('Password dont meet complexity!', 'danger')
+        except Exception:
+            flash('Invalid Inputs', 'danger')
 
-    except Exception as e:
-        print(e)
-    flash('Invalid Inputs', 'danger')
+    def change_password(self, username: str, email: str, password: str) -> bool:
+        try:
+            users = User.find_all_from_db()
+            for user in users:
+                if _decrypt_(username, user._name):
+                    if Password.confirm_password(password, username, update=True):
+                        Password._update_to_db(username,
+                                               password)
+                        flash('Password Changed!', 'danger')
+                        return True
 
-    return redirect(url_for('users.register_get'))
+            flash('User not Exists\nor\nPassword dont meet complexity!',
+                  'danger')
 
+        except Exception as e:
+            flash('Invalid Inputs', 'danger')
 
-def _change_password(username: str, email: str, password: str):
-    try:
-        users = User.find_all_from_db()
-        for user in users:
-            if _decrypt_(username, user._name):
-                if Password.confirm_password(password, username, update=True):
-                    Password._update_to_db(username,
-                                           password)
-                    flash('Password Changed!', 'danger')
-                    return redirect(url_for('users.login_get'))
-        flash('User not Exists\nor\nPassword dont meet complexity!',
-              'danger')
-
-    except Exception as e:
-        print(e)
-        flash('Invalid Inputs', 'danger')
-
-    return redirect(url_for('users.change_password_get'))
-
-
-def logout():
-    if session['name_email']:
-        flash(f"Goodbye {session['name_email']}", 'danger')
-        session['name_email'] = None
+    def logout(self):
+        if session['name_email']:
+            flash(f"Goodbye {session['name_email']}", 'danger')
+            session['name_email'] = None
